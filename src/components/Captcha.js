edc423d83f47d58ccd29584e27386256692c42a8
milad asghari <miladasghari2016@gmail.com>
2018-04-14 10:45:59 +0430

    code review captcha.js

--- src/components/Captcha.js
+++ src/components/Captcha.js
@@ -34,7 +34,6 @@ export default class Captcha extends Component {
       console.log("this is false");
       await this.setState({isVisible : false});
     }
-    // this.setState({isVisible : true});
     return ;
   }
   render() {
@@ -56,25 +55,25 @@ export default class Captcha extends Component {
           transparent={true}
           visible={show && this.state.isVisible }
           onRequestClose={() => {this.setState({isVisible : true})}}>
-         <View style={{justifyContent:'center',backgroundColor : 'rgba(37, 37, 37,0.9)',alignItems : 'stretch',flex : 1}}>
-          <View>
-            <View style={{bottom : 100,alignItems :'center'}}>
-              <LinearText text={text}/>
+           <View style={{justifyContent:'center',backgroundColor : 'rgba(37, 37, 37,0.9)',alignItems : 'stretch',flex : 1}}>
+             <View>
+               <View style={{bottom : 100,alignItems :'center'}}>
+                 <LinearText text={text}/>
+               </View>
+               <Text style={[{textAlign:'center',color : '#6572b2',bottom : 80,fontSize:30}, isFontLoaded && {fontFamily : 'Montserrat-Bold' }]}>Click Or Touch The {this.props.buttons[rand]}</Text>
+               <View style={{flexDirection : 'row',marginLeft : 20,marginRight : 20}}>
+                  {this.props.buttons.map((button,i)=>{
+                  return(
+                  <TouchableOpacity key={i} onPress={this.validateText.bind(this,i)} style={{flex : 1}}>
+                    <Text style={[{textAlign:'center',color : '#fff',fontSize : 20}, isFontLoaded && {fontFamily : 'Montserrat-Bold' }]}>{button}</Text>
+                  </TouchableOpacity>
+                  ) 
+                  })}
+                </View>  
+              </View>
             </View>
-            <Text style={[{textAlign:'center',color : '#6572b2',bottom : 80,fontSize:30}, isFontLoaded && {fontFamily : 'Montserrat-Bold' }]}>Click Or Touch The {this.props.buttons[rand]}</Text>
-            <View style={{flexDirection : 'row',marginLeft : 20,marginRight : 20}}>
-              {this.props.buttons.map((button,i)=>{
-               return(
-                <TouchableOpacity key={i} onPress={this.validateText.bind(this,i)} style={{flex : 1}}>
-                  <Text style={[{textAlign:'center',color : '#fff',fontSize : 20}, isFontLoaded && {fontFamily : 'Montserrat-Bold' }]}>{button}</Text>
-                </TouchableOpacity>
-               ) 
-              })}
-            </View>  
-          </View>
-         </View>
-        </Modal>
-      </View>//514a9d
+          </Modal>
+      </View>
     );
   }
 }
