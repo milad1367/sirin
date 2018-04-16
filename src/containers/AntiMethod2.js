d940beb196fe8e6379aec90df8a3950cf21fbe7e
milad asghari <miladasghari2016@gmail.com>
2018-04-16 15:35:29 +0430

    review antihack2

--- src/containers/AntiMethod2.js
+++ src/containers/AntiMethod2.js
@@ -1,40 +1,22 @@
-//import { CalcDistance } from "./CalcDistance";
 import setPedoSteps from '../actions'
 const AntiMethod2 = (steps,StartPos,LocalEndPos ) => {
-
      let p,x,min,max,StepsDotX;
      p = 0.6 ; 
      X = 0.4;
-
      let Distance = CalcDistance(StartPos.coords.latitude,StartPos.coords.longitude,LocalEndPos.coords.latitude,LocalEndPos.coords.longitude)*1000;
      min = (Distance - (Distance*p));
      max = (Distance + (Distance*p));
      StepsDotX = steps * X ;
-     console.log("on method 2 distance="+Distance+" steps="+steps);
-     console.log("min="+min+" max="+max+" StepsDotX="+StepsDotX);
-     console.log("StartPos input method2");
-     console.log(StartPos);
-     console.log("LocalEndPos input method2");
-     console.log(LocalEndPos);
       if ( StepsDotX < min || StepsDotX > max ) {
-        console.log ( "acces denied with method 2");
         return false
-      }
-    
-           
-      console.log("method 2 pass ");
+      }       
       PostSteps(steps);
       return true
 }
-
 export default AntiMethod2
-
-
 async function PostSteps(mysteps) {
   try{
     const UserInfo = await AsyncStorage.getItem("@user_info");
-  //  UserInfo = JSON.parse(UserInfo);
-    console.log(UserInfo);
     fetch('https://sirin-app.herokuapp.com/user/steps/save', {
       method: 'POST',
       headers: {
@@ -49,7 +31,6 @@ async function PostSteps(mysteps) {
 }
 catch(e){
     console.log('caught error', e);
-    // Handle exceptions
 }
  
 }
